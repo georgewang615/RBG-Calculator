54745cc6d994b87b119791c7fa70e72285c326aa
George Wang <gwan6051@uni.sydney.edu.au>
2021-04-26 14:37:25 +1000

    FINAL FINAL

--- py_functions.py
+++ py_functions.py
@@ -5,63 +5,67 @@ def py_r_psnr(image1, image2):
 
     for i in range(len(image1)): #iterating through each line of pixels
         MSE += (image1[i][0]-image2[i][0]) ** 2 #adding each squared difference to MSE
-    MSE /= len(image1) #divinding MSE by image length
+    MSE /= len(image1) #dividing MSE by image length
 
     try:
         PSNR = 10 * math.log10((255**2)/MSE)
 
     except ZeroDivisionError: #prevents zero division where images are identical
-        PSNR = 0
+        PSNR = float(0)
+    
+    return PSNR
+    
 
-    print("Red PSNR: {}".format(PSNR))
 
 def py_g_psnr(image1, image2):
     MSE = 0
 
-    for i in range(len(image1)):
-        MSE += (image1[i][0]-image2[i][0]) ** 2 
-    MSE /= len(image1)
+    for i in range(len(image1)): 
+        MSE += (image1[i][1]-image2[i][1]) ** 2 
+    MSE /= len(image1) 
 
     try:
         PSNR = 10 * math.log10((255**2)/MSE)
 
-    except ZeroDivisionError:
-        PSNR = 0
+    except ZeroDivisionError: 
+        PSNR = float(0)
 
-    print("Green PSNR: {}".format(PSNR))
+    return PSNR
+    
+
 
 def py_b_psnr(image1, image2):
     MSE = 0
 
-    for i in range(len(image1)):
-        MSE += (image1[i][0]-image2[i][0]) ** 2 
-    MSE /= len(image1)
+    for i in range(len(image1)): 
+        MSE += (image1[i][2]-image2[i][2]) ** 2 
+    MSE /= len(image1) 
 
     try:
         PSNR = 10 * math.log10((255**2)/MSE)
 
-    except ZeroDivisionError:
+    except ZeroDivisionError: 
         PSNR = 0
 
-    print("Blue PSNR: {}".format(PSNR))
+    return PSNR
+
 
 def py_total_psnr(image1, image2):
+    MSE = 0
 
     if len(image1[0]) == 1: #case where images are monochrome
-        MSE = 0
 
         for i in range(len(image1)):
-            MSE += (image1[i]-image2[i]) ** 2
+            MSE += (image1[i][0]-image2[i][0]) ** 2
         MSE /= len(image1)
 
         try:
             PSNR = 10 * math.log10((255**2)/MSE)
 
         except ZeroDivisionError:
-            PSNR = 0
+            PSNR = float(0)
     else: #case where images are coloured
-        MSE = 0
-
+    
         for i in range(len(image1)):
 
             for j in range(3):
@@ -73,6 +77,6 @@ def py_total_psnr(image1, image2):
             PSNR = 10 * math.log10((255**2)/MSE)
 
         except ZeroDivisionError:
-            PSNR = 0
+            PSNR = float(0)
 
-    print("PSNR of images: {}".format(PSNR))
+    return PSNR
